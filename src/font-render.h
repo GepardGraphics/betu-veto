949779b78e1879a209fa05ceb613db3bf0025009
Szilard Ledan <szledan@gmail.com>
2017-07-25 15:51:06 +0200

    Add main header file
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

---
+++ src/font-render.h
@@ -0,0 +1,11 @@
+/* Copyright (C) 2017 Dániel Bátyai dbatyai@inf.u-szeged.hu
+ * Copyright (C) 2017 Szilard Ledan szledan@gmail.com
+ *
+ * See the root LICENSE.
+ *
+ */
+
+#ifndef FONT_RENDER_H
+#define FONT_RENDER_H
+
+#endif FONT_RENDER_H
